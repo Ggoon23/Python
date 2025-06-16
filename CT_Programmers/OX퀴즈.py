641812a2f6f1a1d0e86ab90952ce79ea183345ff
김태곤 <xogoon@naver.com>
2025-06-16 10:35:11 +0900

    0616

--- CT_Programmers/OX퀴즈.py
+++ CT_Programmers/OX퀴즈.py
@@ -1,11 +1,6 @@
 quiz = ["19 - 6 = 13", "5 + 66 = 71", "5 - 15 = 63", "3 - 1 = 2"]
 
 def solution(quiz):
-    answer = []
-    for q in quiz :
-        if int(q) == True :
-            answer.append("O")
-        else :
-            answer.append("X")
-    return answer
+    return ["O" if eval(q.split('=')[0]) == int(q.split('=')[1]) else "X" for q in quiz]
+
 print(solution(quiz))
