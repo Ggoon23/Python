672d69a0bf6135a909639cfbdd74137bff90b3ef
Ggoon23 <xogoon@naver.com>
2025-06-27 00:45:02 +0900

    0627

--- CT_Programmers/외계어 사전.py
+++ CT_Programmers/외계어 사전.py
@@ -1,15 +1,16 @@
-spell = ["p", "o", "s"]
-dic = ["sod", "eocd", "qixm", "adio", "soo"]
+spell = ["z", "d", "x"]
+dic = ["def", "dww", "dzx", "loveaw"]
+
 
 def solution(spell, dic):
-    answer = 0
+    answer = 2
     for i in dic :
         count = 0
-        for j in spell :
-            if j in i :
+        for j in spell:
+            if i.count(j) > 0 :
                 count += 1
-        if count == len(i) :
-            answer += 1
+        if count == len(spell) :
+            answer = 1
     return answer
 
 print(solution(spell,dic))
